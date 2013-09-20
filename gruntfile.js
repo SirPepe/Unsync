e7e3415772d68c52e8aa62f44de6c207a5c636aa
Peter Kröner <peter@peterkroener.de>
2013-09-20 17:29:30 +0200

    PhantomJS is way to bad with HTML5 to be of any use for testing

--- gruntfile.js
+++ gruntfile.js
@@ -11,34 +11,12 @@ uglify: {
       'dist/unsync.min.js': 'src/unsync.js'
     }
   }
-},
-
-connect: {
-  server: {
-    options: {
-      port: 23456
-    }
-  }
-},
-
-qunit: {
-  all: {
-    options: {
-      urls: (function(){
-        var files = grunt.file.expand('test/*.html');
-        return files.map(function(file){
-          return 'http://localhost:23456/' + file;
-        });
-      })()
-    }
-  }
 }
 
 });
 
 grunt.loadNpmTasks('grunt-contrib');
 
-grunt.registerTask('test',    ['connect', 'qunit']);
-grunt.registerTask('default', ['test', 'uglify']);
+grunt.registerTask('default', ['uglify']);
 
 };
