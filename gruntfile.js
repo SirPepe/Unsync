a810e3e1e988804b800116b5447493034bc3f7bf
Peter Kröner <peter@peterkroener.de>
2013-10-03 10:37:14 +0200

    Jshint

--- gruntfile.js
+++ gruntfile.js
@@ -4,6 +4,9 @@ grunt.initConfig({
 
 pkg: grunt.file.readJSON('package.json'),
 
+jshint: {
+  all: ['gruntfile.js', 'src/**/*.js', 'test/**/*.js']
+},
 
 uglify: {
   all: {
@@ -17,6 +20,6 @@ uglify: {
 
 grunt.loadNpmTasks('grunt-contrib');
 
-grunt.registerTask('default', ['uglify']);
+grunt.registerTask('default', ['jshint', 'uglify']);
 
 };
--- src/unsync.js
+++ src/unsync.js
@@ -10,7 +10,8 @@ var slice = Function.prototype.call.bind(Array.prototype.slice);
 var URL = window.URL || window.webkitURL;
 
 function unsync(sourceFn, autoTerminate){
-  if(typeof sourceFn !== 'function') throw new Error('Expected function, got ' + typeof sourceFn);
+  if(typeof sourceFn !== 'function') throw new Error('Expected function, got ' +
+    typeof sourceFn);
   var sourceCode = unsync.createBlobTemplate(sourceFn.toString());
   var worker = unsync.createWorker(sourceCode);
   var fn = unsync.createAsyncFunction(worker, Boolean(autoTerminate));
@@ -36,10 +37,10 @@ Object.defineProperty(unsync, 'supported', {
   }
 });
 
-unsync.template = 'this.onmessage = function(evt){\
-  var result = (#CODE#).apply(null, evt.data);\
-  this.postMessage(result);\
-};';
+unsync.template = 'this.onmessage = function(evt){' +
+'  var result = (#CODE#).apply(null, evt.data);' +
+'  this.postMessage(result);' +
+'};';
 
 unsync.createBlobTemplate = function(code){
   return this.template.split('#CODE#').join(code);
@@ -56,7 +57,7 @@ unsync.createWorker = function(code){
 unsync.createAsyncFunction = function(worker, autoTerminate){
   var isTerminated = false;
   var fn = function fn(){
-    if(isTerminated) throw new Error('Background process was already terminated');
+    if(isTerminated) throw new Error('Background process already terminated');
     var done = (arguments.length > 0) ? arguments[arguments.length -1] : null;
     var args = (arguments.length > 1) ? slice(arguments, 0, -1) : [];
     worker.postMessage(args);
